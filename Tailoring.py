58f7820a0e775c734c91bd54b77781ebb876767d
feboss <feboss86@gmail.com>
2018-07-20 02:45:38 +0200

    0.1

--- Tailoring.py
+++ Tailoring.py
@@ -10,12 +10,13 @@
 
 sewingKitID=0x0F9D
 gumpStuddedArmor=50
-gumpStuddedGorget=2 
+gumpStuddedGorget=2
+gumpStuddedGloves=9
+gumpStuddedSleeves=16
+gumpStuddedLeggins=23
+gumpStuddedTunic=30
+gumpStuddedDo=44
 cassaLavoro=0x40066CAA
-gumpPlateGloves=58 #fino a 108.9
-gumpPlateArms=51 #fino a 116.3
-gumpPlateLegs=72 #fino a 118.8
-gumpPlateArmor=79 #120
 
 
 def rifornisci(cassaPelli):
@@ -35,7 +36,7 @@ def Taglia(oggettoID):
             Target.WaitForTarget(10000, False)
             Target.TargetExecute(item)
             
-def Crafta(sewingKitID,gumpID):
+def Crafta(sewingKitID,gumpID,itemID):
     cassaPelli=0x404A31F2
     leatherID=0x1081
     gorgetID=0x13D6
@@ -45,13 +46,14 @@ def Crafta(sewingKitID,gumpID):
     Gumps.WaitForGump(949095101, 10000)
     Journal.Clear()
     while True:
-        if (Player.Weight > Player.MaxWeight - 80): 
-            break
+        '''if (Player.Weight > Player.MaxWeight - 80):
+            if(Items.BackpackCount(gorgetID, 0)<1):
+                break'''
         if (int(((Items.GetPropStringList(Player.Backpack)[2].split('/', 1)[0]))[10:])>120):
             break
         if(Items.BackpackCount(leatherID, 0)<20):
-            if(Items.BackpackCount(gorgetID, 0)>10):
-                Taglia(gorgetID)
+            if(Items.BackpackCount(itemID, 0)>10):
+                break
             else:
                 rifornisci(cassaPelli)               
         if (Journal.Search("You have worn out your tool!") or (Journal.Search("That container cannot") )):
@@ -67,35 +69,40 @@ def Crafta(sewingKitID,gumpID):
 def main():
     cassaPelli=0x404A31F2
     gorgetID=0x13D6
+    glovesID=0x13D5
+    sleevesID=0x13D4
+    legginsID=0x13DA
+    tunicID=0x13DB
+    doID=0x27C7
     sewingKitID=0x0F9D
     while Player.GetSkillValue("Tailoring")>=99.6 and Player.GetSkillValue("Tailoring")<103.7:
         #makeMartello()
         #makeTinkerTool()
-        Crafta(sewingKitID,gumpStuddedGorget)
+        Crafta(sewingKitID,gumpStuddedGorget,gorgetID)
         Taglia(gorgetID)
-    '''while Player.GetSkillValue("Tailoring")>=103.7 and Player.GetSkillValue("Tailoring")<107.9:
-        makeMartello()
-        makeTinkerTool()
-        Crafta(gumpPlateGloves)
-        Smelta(glovesID)
+    while Player.GetSkillValue("Tailoring")>=103.7 and Player.GetSkillValue("Tailoring")<107.9:
+        #makeMartello()
+        #makeTinkerTool()
+        Crafta(sewingKitID,gumpStuddedGloves,glovesID)
+        Taglia(glovesID)
     while Player.GetSkillValue("Tailoring")>=107.9 and Player.GetSkillValue("Tailoring")<111.9:
-        makeMartello()
-        makeTinkerTool()
-        Crafta(gumpPlateArms)
-        Smelta(armsID)
+        #makeMartello()
+        #makeTinkerTool()
+        Crafta(sewingKitID,gumpStuddedSleeves,sleevesID)
+        Taglia(sleevesID)
     while Player.GetSkillValue("Tailoring")>=111.9 and Player.GetSkillValue("Tailoring")<115.9:
-        makeMartello()
-        makeTinkerTool()
-        Crafta(gumpPlateLegs)
-        Smelta(legsID)
+        #makeMartello()
+        #makeTinkerTool()
+        Crafta(sewingKitID,gumpStuddedLeggins,legginsID)
+        Taglia(legginsID)
     while Player.GetSkillValue("Tailoring")>=115.9 and Player.GetSkillValue("Tailoring")<118.9: 
-        makeMartello()
-        makeTinkerTool()
-        Crafta(gumpPlateArmor)
-        Smelta(armorID) 
+        #makeMartello()
+        #makeTinkerTool()
+        Crafta(sewingKitID,gumpStuddedTunic,tunicID)
+        Taglia(tunicID) 
     while Player.GetSkillValue("Tailoring")>=118.9 and Player.GetSkillValue("Tailoring")<120: 
-        makeMartello()
-        makeTinkerTool()
-        Crafta(gumpPlateArmor)
-        Smelta(armorID) '''
+        #makeMartello()
+        #makeTinkerTool()
+        Crafta(sewingKitID,gumpStuddedDo,doID)
+        Taglia(doID) 
 main()
