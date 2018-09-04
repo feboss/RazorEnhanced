d37a2835c31b4950f289efa77d9b9eb8aadd3fd8
feboss <feboss@users.noreply.github.com>
2018-09-04 23:28:30 +0200

    Delete operazioneFile.py

--- operazioneFile.py
+++
@@ -1,6 +0,0 @@
-#Player.HeadMessage(30,str(Player.Position))
-#1000,518,-29
-#Player.PathFindTo(1000,518,-29)
-filename = "test.txt"
-with open(filename, 'w') as f:
-            f.write(str(Player.Position)[1:-1])
