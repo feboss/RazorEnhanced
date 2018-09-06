ba7efe564193cb577c0a4b9464837e7db11819fc
feboss <feboss@users.noreply.github.com>
2018-09-06 15:49:33 +0200

    Update MaceAndShield.py

--- MaceAndShield.py
+++ MaceAndShield.py
@@ -72,13 +72,14 @@ def craftaBuckler():
     Gumps.WaitForGump(949095101, 10000)
     Gumps.SendAction(949095101, 15)
     Gumps.WaitForGump(949095101, 10000)
+    if Items.BackpackCount(ingotID,-1)<10:#Se non crafti
+        Misc.Pause(2000)
     while(Items.BackpackCount(ingotID,-1)>=10):                     
         Gumps.SendAction(949095101, 2)  
         if Journal.Search("You have worn"):
             Journal.Clear()
             Items.UseItemByID(martelloID,-1)#mar       
-        Gumps.WaitForGump(949095101, 5000)
-                            
+        Gumps.WaitForGump(949095101, 5000)                            
     Gumps.SendAction(949095101, 0) #exit
 
 def goBank():
@@ -96,10 +97,10 @@ def goBank():
     while not Journal.Search("Bank container has"):
         Player.ChatSay(52, "bank")
         Misc.Pause(200)
-    if Items.BackpackCount(ingotID,-1)<=10:
+    while Items.BackpackCount(ingotID,-1)<numeroScudi*10:
         for item in Player.Bank.Contains:
             if item.ItemID == 0x1BF2:
-                Items.Move(item, Player.Backpack, numeroScudi*10)
+                Items.Move(item, Player.Backpack, numeroScudi*10-Items.BackpackCount(ingotID,-1) )
                 Misc.Pause(2000)
                 break
     else:
@@ -124,13 +125,14 @@ def goFabbro():
     Gumps.WaitForGump(1431013363, 10000)
     Gumps.SendAction(1431013363, runaFabbro*6-1)
     while Player.Position.X == x and Player.Position.Y == y:
-        Misc.Pause(200)
-    Misc.Pause(200)
+        Misc.Pause(100)
+    Misc.Pause(100)
     
 def consegna():
-    while not Mobiles.FindBySerial(0x000377D0): Misc.NoOperation()
-    Mobiles.UseMobile(0x000377D0)
-    Gumps.WaitForGump(2510313894, 10000)
+    while not Mobiles.FindBySerial(0x000377D0): Misc.Pause(50)
+    while not Gumps.HasGump( ):
+        Mobiles.UseMobile(0x000377D0)
+        Gumps.WaitForGump(2510313894, 10000)
     for item in Player.Backpack.Contains:
         if item.ItemID==bucklerID:
             Gumps.SendAction(2510313894, 304)
@@ -149,5 +151,5 @@ def main():
         goBritain()
         consegna()
         Misc.Pause(500)
-        
+     
 main()
